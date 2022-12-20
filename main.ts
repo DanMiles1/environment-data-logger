b7a1633e5c63af70fb004ed6c7d8a949ffd87811
Dan <49734943+DanMiles1@users.noreply.github.com>
2022-12-20 07:48:41 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ datalogger.onLogFull(function () {
         `)
 })
 input.onButtonPressed(Button.A, function () {
+    serial.writeLine("A")
     basic.showNumber(input.temperature())
 })
 input.onButtonPressed(Button.AB, function () {
@@ -17,38 +18,84 @@ input.onButtonPressed(Button.AB, function () {
     datalogger.setColumnTitles(
     "temperature",
     "light",
-    "sound"
+    "rcvdserial",
+    "temperature1",
+    "light1"
     )
 })
 input.onButtonPressed(Button.B, function () {
-    if (on == 0) {
+    serial.writeLine("B")
+    if (logging == false) {
         logging = true
         basic.showIcon(IconNames.Yes)
-        on = 1
     } else {
         logging = false
         basic.showIcon(IconNames.No)
-        on = 0
     }
 })
+radio.onReceivedValue(function (name, value) {
+    if (name == "temp") {
+        temp1 = value
+        rcvdserial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    } else if (name == "light") {
+        light1 = value
+        rcvdserial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    }
+})
+let light1 = 0
+let rcvdserial = 0
+let temp1 = 0
 let logging = false
-let on = 0
-on = 0
+radio.setGroup(2)
+radio.setTransmitPower(7)
+radio.setTransmitSerialNumber(true)
 logging = false
-basic.showIcon(IconNames.No)
 datalogger.setColumnTitles(
 "temperature",
 "light",
-"sound"
+"rcvdserial",
+"temperature1",
+"light1"
 )
+basic.showIcon(IconNames.Heart)
 loops.everyInterval(60000, function () {
-    if (logging) {
-        basic.showIcon(IconNames.Heart)
+    if (logging == true) {
+        serial.redirectToUSB()
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
         datalogger.log(
         datalogger.createCV("temperature", input.temperature()),
         datalogger.createCV("light", input.lightLevel()),
-        datalogger.createCV("sound", input.soundLevel())
+        datalogger.createCV("light1", light1),
+        datalogger.createCV("temperature1", temp1),
+        datalogger.createCV("rcvdserial", rcvdserial)
         )
-        basic.clearScreen()
+        serial.writeValue("temp", input.temperature())
+        serial.writeValue("light", input.lightLevel())
+        serial.writeValue("rcvdserial", rcvdserial)
+        serial.writeValue("temp1", temp1)
+        serial.writeValue("light1", light1)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        radio.sendValue("temp", input.temperature())
+        radio.sendValue("light", input.lightLevel())
     }
 })
